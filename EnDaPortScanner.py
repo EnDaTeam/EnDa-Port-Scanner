438ee111403f40c683c423682a8bf258c8e137ec
EnDa Company <102385844+EnDaTeam@users.noreply.github.com>
2022-12-03 22:21:46 +0200

    Add files via upload

--- EnDaPortScanner.py
+++ EnDaPortScanner.py
@@ -14,67 +14,78 @@ if os.name in ("dos","nt"):
 morebanner(random.randint(1,7))
 enter()
 slowPrint(Fore.CYAN + "[$] Welcome to EnDa Port Scanner made by EnDaTeam!" + Fore.RESET,times=0.08)
-enter()
-checked = 0
-opened = 0
-while True:
-    tip("Please enter the hostname!",option=1)
+mainp = 1
+while mainp:
     enter()
-    host = input(Fore.YELLOW + "[+] Host >> " + Fore.CYAN)
-    ports = input(Fore.YELLOW + "[+] Port limit >> " + Fore.MAGENTA)
-    speed = input(Fore.YELLOW + "[+] Speed (seconds) >> " + Fore.GREEN)
+    checked = 0
+    opened = 0
+    while True:
+        tip("Please enter the hostname!",option=1)
+        tip("Leave blank the port limit if you want to scan a specific port!",option=1)
+        enter()
+        host = input(Fore.YELLOW + "[+] Host >> " + Fore.CYAN)
+        ports = input(Fore.YELLOW + "[+] Port limit >> " + Fore.MAGENTA)
+        speed = input(Fore.YELLOW + "[+] Speed (seconds) >> " + Fore.GREEN)
+        enter()
+        if hostname_resolves(host):
+            break
+        else:
+            error("The inputed host does not exist!",option=1)
+            enter()
+            time.sleep(2)
+        try:
+            int(ports)
+        except:
+            error("The inputed port limit is not avaible!",option=1)
+            enter()
+            time.sleep(2)
+        try:
+            float(speed)
+        except:
+            error("The inputed speed is not avaible!",option=1)
+            enter()
+            time.sleep(2)
+    target = socket.gethostbyname(host) 
+    print(Colorate.Horizontal(Colors.white_to_green,f"[!] Starting the scan >> {datetime.today()}",2))
+    print(Colorate.Horizontal(Colors.white_to_blue,f"[!] Starting the scan >> {target}",2))
     enter()
-    if hostname_resolves(host):
-        break
-    else:
-        error("The inputed host does not exist!",option=1)
-        enter()
-        time.sleep(2)
     try:
-        int(ports)
-    except:
-        error("The inputed port limit is not avaible!",option=1)
-        enter()
+        for port in range(1,int(ports) + 1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(float(speed))
+            result = s.connect_ex((target,port))
+            if result == 0:
+                print(Fore.GREEN + f"[+] Port {port} is open" + Fore.RESET)
+                opened = opened + 1
+            #else:
+            #    print(Fore.RED + f"[-] Port {port} is closed" + Fore.RESET)
+            checked = checked + 1
+            if os.name in ("dos","nt"):
+                os.system(f"title EnDa Port Scanner ^| EnDaTeam ^| Checked : {checked} ^| Opened : {opened}")
+            s.close()
+        if opened == 0:
+            print(Fore.RED + f"[-] The host [{target}] does not have any opened port!" + Fore.RESET)
         time.sleep(2)
-    try:
-        float(speed)
-    except:
-        error("The inputed speed is not avaible!",option=1)
-        enter()
-        time.sleep(2)
-target = socket.gethostbyname(host) 
-print(Colorate.Horizontal(Colors.white_to_green,f"[!] Starting the scan >> {datetime.today()}",2))
-print(Colorate.Horizontal(Colors.white_to_blue,f"[!] Starting the scan >> {target}",2))
-enter()
-try:
-    for port in range(1,int(ports) + 1):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(float(speed))
-        result = s.connect_ex((target,port))
-        if result == 0:
-            print(Fore.GREEN + f"[+] Port {port} is open" + Fore.RESET)
-            opened = opened + 1
-        #else:
-        #    print(Fore.RED + f"[-] Port {port} is closed" + Fore.RESET)
-        checked = checked + 1
-        if os.name in ("dos","nt"):
-            os.system(f"title EnDa Port Scanner ^| EnDaTeam ^| Checked : {checked} ^| Opened : {opened}")
-        s.close()
-    if opened == 0:
-        print(Fore.RED + f"[-] The host [{target}] does not have any opened port!" + Fore.RESET)
-    time.sleep(5)
-    exit()
-except KeyboardInterrupt:
-        enter()
-        error("Exiting Program by Shortcut CTRL-C!",option=1)
-        time.sleep(3)
-        exit()
-except socket.gaierror:
-        enter()
-        error(f"Hostname [{target}] Could Not Be Resolved !",option=1)
-        time.sleep(3)
-        exit()
-except socket.error:
-        enter()
-        error(f"Server [{target}] not responding {404}!",option=1)
-        exit()
+        enter()   
+        repetitive = input(Colorate.Horizontal(Colors.red_to_white,"[!] Do you want to do another scan? >> ",2))
+        if repetitive.lower() in ("1","y","yeah","yes","","ye","yep"):
+            continue
+        else:
+            enter()
+            print(Fore.RED + "[-] Roger that, exiting program.." + Fore.RESET)
+            time.sleep(3)
+            exit()
+    except KeyboardInterrupt:
+            enter()
+            error("Exiting Program by Shortcut CTRL-C!",option=1)
+            time.sleep(3)
+            exit()
+    except socket.gaierror:
+            enter()
+            error(f"Hostname [{target}] Could Not Be Resolved !",option=1)
+            time.sleep(3)
+            exit()
+    except socket.error:
+            enter()
+            error(f"Server [{target}] not responding {404}!",option=1)
+            exit()
